aaf9e38e11739bf21680d721244392cab1a5e031
Rémi Campistron <remi.campistron@gmail.com>
2025-12-19 13:18:53 +0100

    Création des getters/setters et du constructeur

--- src/bank/models/User.java
+++ src/bank/models/User.java
@@ -1,4 +1,27 @@
 package bank.models;
 
 public class User {
+    String firstName;
+    String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
