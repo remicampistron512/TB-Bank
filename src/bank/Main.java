9a6255ecb954d62c1b5f118c18e6a8dff1e54bbd
Rémi Campistron <remi.campistron@gmail.com>
2025-12-19 22:52:53 +0100

    Connexion a la bdd

---
+++ src/bank/Main.java
@@ -0,0 +1,7 @@
+import bank.daos.CustomerDao;
+import bank.models.Customer;
+
+void main() {
+    CustomerDao customerDao = new CustomerDao();
+    //customerDao.createCustomer("bobby","bob");
+}
---
+++ src/bank/daos/DaoException.java
@@ -0,0 +1,10 @@
+package bank.daos;
+
+public class DaoException extends RuntimeException {
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public DaoException(String message) {
+        super(message);
+    }
+}
