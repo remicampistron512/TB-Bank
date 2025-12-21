ef7f20c03705d7e7b8ccd76df93288eee6bfa4ed
Rémi Campistron <remi.campistron@gmail.com>
2025-12-21 19:38:13 +0100

    Affichage du menu clients et comptes

--- src/bank/ui/ConsoleMenus.java
+++ src/bank/ui/ConsoleMenus.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class ConsoleMenus {
   private final Scanner in = new Scanner(System.in);
+  private static final String CHOICE_TEXT = "Choose: ";
   private final BankingService service;
   public ConsoleMenus(BankingService service) {
     this.service = service;
@@ -23,7 +24,7 @@ public class ConsoleMenus {
       System.out.println("2) Accounts");
       System.out.println("0) Exit");
 
-      int choice = readInt("Choose: ", 0, 2);
+      int choice = readInt(CHOICE_TEXT, 0, 2);
       switch (choice) {
         case 1 -> customersMenu();
         case 2 -> accountsMenu();
@@ -47,9 +48,57 @@ public class ConsoleMenus {
       }
     }
   }
+
   private void accountsMenu() {
+    while (true) {
+      System.out.println("\n--- Accounts ---");
+      System.out.println("1) Open account");
+      System.out.println("2) Deposit");
+      System.out.println("3) Transfer");
+      System.out.println("0) Back");
+
+      int choice = readInt(CHOICE_TEXT, 0, 3);
+      switch (choice) {
+        case 1 -> openAccount();
+        case 2 -> deposit();
+        case 3 -> transfer();
+        case 0 -> { return; }
+      }
+    }
+  }
+
+  private void transfer() {
+  }
+
+  private void deposit() {
+    
+  }
+
+  private void openAccount() {
+
   }
 
   private void customersMenu() {
+    while (true) {
+      System.out.println("\n--- Customers ---");
+      System.out.println("1) List customers");
+      System.out.println("2) Create customer");
+      System.out.println("0) Back");
+
+      int choice = readInt(CHOICE_TEXT, 0, 2);
+      switch (choice) {
+        case 1 -> listCustomers();
+        case 2 -> createCustomer();
+        case 0 -> { return; } // back to main menu
+      }
+    }
   }
+
+  private void listCustomers() {
+  }
+
+  private void createCustomer() {
+    
+  }
+
 }
